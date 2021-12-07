4ad4c6a026a4319ccdb680f508fc55e1ceddb5af
Opilane TTHK <opilane@tthk.ee>
2021-12-07 16:52:29 +0200

    first commit

---
+++ BitcoinCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitcoinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
